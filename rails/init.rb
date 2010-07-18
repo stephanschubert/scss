247ec9a2ac920e3a82676113abb37481b959dc60
jazen <jazen@vm1.(none)>
2010-07-18 17:44:48 +0200

    Fix setup

--- rails/init.rb
+++ rails/init.rb
@@ -1,2 +1,4 @@
-Dir.chdir("..")
-FileUtils.ln_sf Dir.pwd, Rails.root + "public/stylesheets/sass/scss"
+src = Rails.root + "vendor/plugins/scss"
+dst = Rails.root + "public/stylesheets/sass/scss"
+
+FileUtils.ln_sf src, dst
