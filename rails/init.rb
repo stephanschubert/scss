bbccd7ef1401a47b1eb1cece5736eb1824eafbd0
jazen <jazen@vm1.(none)>
2010-07-29 18:31:35 +0200

    Add custom functions on start up

--- rails/init.rb
+++ rails/init.rb
@@ -2,3 +2,6 @@ src = Rails.root + "vendor/plugins/scss"
 dst = Rails.root + "public/stylesheets/sass/scss"
 
 FileUtils.ln_sf src, dst
+
+require "../lib/sass_extensions/functions/urls"
+Sass::Script::Functions.send :include, SassExtensions::Functions::Urls
